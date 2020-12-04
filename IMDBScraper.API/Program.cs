7be9e658f13f21407a7ae56a42f4ea944ac42e6f
alexey epp <alexepp@gmail.com>
2020-12-04 14:41:21 +0200

    Add Elasticsearch logs

--- IMDBScraper.API/Program.cs
+++ IMDBScraper.API/Program.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace IMDBScraper.API
 {
@@ -14,26 +16,11 @@ namespace IMDBScraper.API
     {
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
+            //Configure logging first
+            ConfigureLogging();
 
-            Log.Logger = new LoggerConfiguration()
-                             .Enrich.FromLogContext()
-                             .WriteTo.Console()
-                             .CreateLogger();
-
-            try
-            {
-                Log.Information("Starting up");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Application start-up failed");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            //Then create the host, so that if the host fails we can log errors
+            CreateHost(args);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -43,9 +30,51 @@ namespace IMDBScraper.API
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-                .UseSerilog((hostingContext, loggerConfig) =>
-                    loggerConfig.ReadFrom
-                    .Configuration(hostingContext.Configuration)
-                );
+                .UseSerilog( );
+
+        private static void CreateHost(string[] args)
+        {
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (System.Exception ex)
+            {
+                Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+                throw;
+            }
+        }
+
+        private static void ConfigureLogging()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                    optional: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                //.Enrich.FromLogContext()
+                //.Enrich.WithMachineName()
+                //.WriteTo.Debug()
+                //.WriteTo.Console()
+                //.WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            Log.Information("Starting up");
+        }
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+        {
+            return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-new-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+            };
+        }
     }
 }
